16d13c610a4b3de58c3039019c8f129e6657e3a0
82iirriiss <82iirriiss@daum.net>
2020-12-11 23:56:53 +0900

    db 정보 변경

--- config/config.js
+++ config/config.js
@@ -16,10 +16,10 @@ module.exports = {
         dialect: "mysql"
       },
       production: {
-        username: "root",
+        username: "dbmasteruser",
         password: process.env.SEQUELIZE_PASSWORD,
-        database: "nodebird",
-        host: "127.0.0.1",
+        database: "Database-1",
+        host: "ls-bbd465fa70ae045a4409a62376b7e880fcd5bb40.cq9jykrqsudh.ap-northeast-2.rds.amazonaws.com",
         dialect: "mysql",
         logging: false
       }
